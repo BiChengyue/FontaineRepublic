932457dae904563b210668ca6674411a0adb8728
Fireflylover <1978524789@qq.com>
2026-07-25 16:59:46 +0800

    feat(core): initialize core system

--- src/main/java/com/fontainerepublic/FontaineRepublic.java
+++ src/main/java/com/fontainerepublic/FontaineRepublic.java
@@ -1,7 +1,10 @@
 package com.fontainerepublic;
 
+import com.fontainerepublic.core.ConfigManager;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(FontaineRepublic.MOD_ID)
@@ -11,5 +14,11 @@ public class FontaineRepublic {
 
     public FontaineRepublic() {
         LOGGER.info("[FontaineRepublic] Loading");
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);
+    }
+
+    private void onCommonSetup(FMLCommonSetupEvent event) {
+        ConfigManager.load();
+        LOGGER.info("[FontaineRepublic] Core initialized");
     }
 }
